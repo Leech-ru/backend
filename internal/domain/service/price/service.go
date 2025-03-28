35d5714358dc2d43ad3e86d85b79aafaa1fa4b0f
gramrate <prikinok@gmail.com>
2025-03-28 17:50:00 +0300

    fix float types with money

--- internal/domain/service/price/service.go
+++ internal/domain/service/price/service.go
@@ -2,9 +2,9 @@ package price
 
 // CalculateTotal calculates the total cost of the order
 func (s *priceService) CalculateTotal(size1, size2, size3, packageType int) (float64, error) {
-	total := float64(size1)*s.leechPrices[1] +
-		float64(size2)*s.leechPrices[2] +
-		float64(size3)*s.leechPrices[3]
+	total := size1*s.leechPrices[1] +
+		size2*s.leechPrices[2] +
+		size3*s.leechPrices[3]
 
-	return total, nil
+	return float64(total) / 100, nil
 }
--- internal/domain/service/price/total.go
+++ internal/domain/service/price/total.go
@@ -1,15 +1,15 @@
 package price
 
 type priceService struct {
-	leechPrices map[int]float64
+	leechPrices map[int]int
 }
 
 func NewPriceService() *priceService {
 	return &priceService{
-		leechPrices: map[int]float64{
-			1: 105.0,
-			2: 135.0,
-			3: 168.0,
+		leechPrices: map[int]int{
+			1: 10500,
+			2: 13500,
+			3: 16800,
 		},
 	}
 }
