bbb1370ff0898137e03ded438eea1491f5081a29
gramrate <prikinok@gmail.com>
2026-04-12 14:25:35 +0300

    oh my codex (edit origins string)

--- internal/adapters/controller/api/server/server.go
+++ internal/adapters/controller/api/server/server.go
@@ -32,7 +32,7 @@ func Setup(app *app.App) {
 	//app.Server.Use(middleware.Recover())
 
 	app.Server.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"https://t-leech.vercel.app, http://localhost:4200, http://localhost:4000"},
+		AllowOrigins:     []string{"https://t-leech.vercel.app", "http://localhost:4200", "http://localhost:4000"},
 		AllowCredentials: true,
 		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodOptions},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
